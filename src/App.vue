dc7e1ce05c7333e3314154750d48fa93a6737c16
dianaveselkina <vess_di@mail.ru>
2024-11-08 18:35:11 +0300

    create project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MyButton />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MyButton from './components/MyButton.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    MyButton,
+  },
+};
 </script>
 
 <style>
