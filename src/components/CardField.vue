f84bb3e1e18a72ca8246778c5e348aa73fdc3563
dianaveselkina <vess_di@mail.ru>
2024-11-09 22:45:25 +0300

    add img

--- src/components/CardField.vue
+++ src/components/CardField.vue
@@ -18,3 +18,59 @@
   box-shadow: 0 0 14px 14px rgba(65, 62, 21, 1.6);
 }
 </style>
+
+<!-- Показать три случайных изображения
+
+<template>
+  <div>
+    <h2>Случайные изображения</h2>
+    <div class="image-gallery">
+      <img v-for="(image, index) in randomImages" :key="index" :src="image" alt="Случайное изображение" />
+    </div>
+    <button @click="getRandomImages">Показать три случайных изображения</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// Создаем реактивный массив изображений
+const images = ref([
+  'https://example.com/image1.jpg', // Замените на реальные URL ваших изображений
+  'https://example.com/image2.jpg',
+  'https://example.com/image3.jpg',
+  'https://example.com/image4.jpg',
+  'https://example.com/image5.jpg',
+  // Добавьте другие изображения по необходимости
+]);
+
+// Реактивная переменная для хранения случайных изображений
+const randomImages = ref([]);
+
+// Функция для получения трех случайных изображений
+const getRandomImages = () => {
+  const shuffledImages = images.value.sort(() => 0.5 - Math.random()); // Перемешиваем массив
+  randomImages.value = shuffledImages.slice(0, 3); // Берем первые три изображения
+};
+
+// Инициализация при загрузке компонента
+getRandomImages();
+</script>
+
+<style>
+.image-gallery {
+  display: flex;
+  gap: 10px;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+  display: block;
+}
+button {
+  margin-top: 10px;
+  padding: 10px 15px;
+  font-size: 16px;
+}
+</style> -->
--- src/components/MyButton.vue
+++ src/components/MyButton.vue
@@ -9,21 +9,45 @@ const images = ref([
   '/img/card5.png',
   '/img/card6.png',
   '/img/card7.png',
-  '/img/test.png',
+  '/img/card8.png',
+  '/img/card9.png',
+  '/img/card10.png',
+  '/img/card11.png',
+  '/img/card12.png',
+  '/img/card13.png',
+  '/img/card14.png',
+  '/img/card15.png',
+  '/img/card16.png',
+  '/img/card17.png',
+  '/img/card18.png',
+  '/img/card19.png',
+  '/img/card20.png',
+  '/img/card21.png',
+  '/img/card22.png',
 ]);
 
 const randomImage = ref('');
+const randomImage1 = ref('');
 
 const getRandomImage = () => {
   const randomIndex = Math.floor(Math.random() * images.value.length);
   randomImage.value = images.value[randomIndex];
 };
+const getRandomImage1 = () => {
+  const randomIndex = Math.floor(Math.random() * images.value.length);
+  randomImage1.value = images.value[randomIndex];
+};
 </script>
 <template>
   <div>
     <button class="start" @click="getRandomImage">Start</button>
-
-    <img :src="randomImage" v-if="randomImage" />
+    <img
+      :src="randomImage"
+      v-if="randomImage"
+      @click="getRandomImage1"
+      class="prediction"
+    />
+    <img :src="randomImage1" v-if="randomImage1" class="prediction1" />
   </div>
 </template>
 
@@ -39,6 +63,7 @@ const getRandomImage = () => {
   font-size: 1rem;
   color: #472f14;
 }
+
 img {
   position: absolute;
   top: 15rem;
@@ -47,6 +72,18 @@ img {
   width: 6rem;
   height: 8rem;
 }
+.prediction {
+  cursor: pointer;
+}
+.prediction1 {
+  position: absolute;
+  top: 29rem;
+  left: 50%;
+  transform: translate(-50%);
+  width: 6rem;
+  height: 8rem;
+}
+
 .game__start:hover {
   box-shadow: 0 0 14px 14px rgba(65, 62, 21, 1.6);
 }
@@ -72,3 +109,45 @@ img {
   height: 8rem;
 }
 </style>
+<!-- <template>
+  <div>
+    <button @click="handleClick">Нажми меня!</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// Счетчик нажатий на кнопку
+const clickCount = ref(0);
+
+// Определяем массив функций
+const functions = [
+  () => {
+    console.log('Первое нажатие - вызвана функция первого действия');
+    // Ваш код для первого действия
+  },
+  () => {
+    console.log('Второе нажатие - вызвана функция второго действия');
+    // Ваш код для второго действия
+  },
+  () => {
+    console.log('Третье нажатие - вызвана функция третьего действия');
+    // Ваш код для третьего действия
+  },
+];
+
+// Функция для обработки нажатий на кнопку
+const handleClick = () => {
+  const index = clickCount.value % functions.length; // Получить индекс функции
+  functions[index](); // Вызвать соответствующую функцию
+  clickCount.value++; // Увеличить счетчик нажатий
+};
+</script>
+
+<style>
+button {
+  padding: 10px 15px;
+  font-size: 16px;
+}
+</style> -->
